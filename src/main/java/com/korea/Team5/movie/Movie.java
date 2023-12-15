52ee46d084a93a6ed33fb77760fb9a9df84851f0
pogeum <143607484+pogeum@users.noreply.github.com>
2023-12-15 16:51:32 +0900

    movieList

---
+++ src/main/java/com/korea/Team5/movie/Movie.java
@@ -0,0 +1,25 @@
+package com.korea.Team5.movie;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String content;
+
+    private String movieRelease;
+}
