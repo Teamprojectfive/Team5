1482e32d4541fde3f7bd24c5ebf175aafb7b4814
habinnn <107250227+habinnn@users.noreply.github.com>
2023-12-18 12:54:58 +0900

    habin task (#10)

--- src/main/java/com/korea/Team5/movie/MovieController.java
+++ src/main/java/com/korea/Team5/movie/MovieController.java
@@ -1,24 +1,46 @@
 package com.korea.Team5.movie;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.swing.*;
 import java.util.List;
 
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/movie")
 public class MovieController {
 
     private final MovieService movieService;
 
-    @GetMapping("/movie")
-    public String movie(Model model){
-        List<Movie> movieList = this.movieService.list();
-        model.addAttribute("movieList", movieList);
+
+
+
+    @GetMapping("/list")
+    public String movie(Model model, @RequestParam(value="page", defaultValue="0") int page){
+        Page<Movie> paging = this.movieService.list(page);
+        model.addAttribute("paging", paging);
         return "movie";
     }
+
+    @GetMapping("/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Long id){
+        Movie movie = this.movieService.getMovie(id);
+
+
+        model.addAttribute("movie", movie);
+
+
+        return "movieDetail";
+    }
+
 
 
 }
--- src/main/java/com/korea/Team5/movie/MovieRepository.java
+++ src/main/java/com/korea/Team5/movie/MovieRepository.java
@@ -1,8 +1,10 @@
 package com.korea.Team5.movie;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
-
+    Page<Movie> findAll(Pageable pageable);
 
 }
