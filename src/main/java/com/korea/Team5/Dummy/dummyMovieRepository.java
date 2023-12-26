2696a5ffe5526595745f2540b96b6bb105d59e69
habinnn <gkqls7423@gmail.com>
2023-12-26 11:35:11 +0900

    1226api

--- src/main/java/com/korea/Team5/Dummy/dummyMovieRepository.java
+++
@@ -1,7 +0,0 @@
-package com.korea.Team5.Dummy;
-
-import jakarta.persistence.criteria.CriteriaBuilder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface dummyMovieRepository extends JpaRepository<dummyMovie, Integer> {
-}
