689c2972b3dbf77496d87ea311587ec08c88c941
sang5517 <143565832+sang5517@users.noreply.github.com>
2024-01-05 14:27:12 +0900

    Enhancement/2 (#29)
    * login complete test
    
    * close test
    
    * 1218login
    
    * 1218ilha
    
    * hijfskjf
    
    * 1219review
    
    * 1219modifyexception
    
    * 1220
    
    * 1220delete
    
    * 1221fsdfsdf
    
    * 1222
    
    * 1226ilha
    
    * 1227review
    
    * 1227clear
    
    * 1227lastclear
    
    * 1229
    
    * 1229mypage
    
    * 1229clear
    
    * 0102
    
    * 0102email
    
    * 0103modify
    
    * 0103clear
    
    * 0104
    
    * 0104clear
    
    * 0105

--- src/main/java/com/korea/Team5/USER/MemberService.java
+++ src/main/java/com/korea/Team5/USER/MemberService.java
@@ -2,15 +2,19 @@ package com.korea.Team5.USER;
 
 import com.korea.Team5.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class MemberService {
-
+  @Autowired
   private final MemberRepository memberRepository;
 
 
@@ -24,14 +28,17 @@ public class MemberService {
     member.setEmail(email);
     member.setPhone(phone);
 
+    member.setCreateDate(LocalDateTime.now());
+
+
     this.memberRepository.save(member);
 
     return member;
 
   }
 
-  public boolean isDuplicated(String loginId, String email, String nickName, String phone) {
-    return memberRepository.existsByLoginIdOrEmailOrNickNameOrPhone(loginId, email, nickName, phone);
+  public boolean isDuplicated(String loginId, String nickName) {
+    return memberRepository.existsByLoginIdOrNickName(loginId, nickName);
   }
 
 
@@ -46,7 +53,10 @@ public class MemberService {
 
   // 통합 메서드로 소셜 로그인 정보 저장 또는 업데이트
   // 소셜 로그인 정보 저장 또는 업데이트
-  public Member saveOrUpdateSocialMember(String loginId, String socialProvider, String nickName, String name) {
+
+  public Member saveOrUpdateSocialMember(String loginId, String socialProvider, String nickName, String name, LocalDateTime localDateTime) {
+
+
     // 소셜 로그인 사용자 정보를 가져오거나 생성합니다.
     Member member = memberRepository.findByloginId(loginId)
             .orElse(new Member());
@@ -55,8 +65,9 @@ public class MemberService {
     member.setLoginId(loginId);
     member.setNickName(nickName);
     member.setSocialProvider(socialProvider);
-    ;
+
     member.setName(name);
+    member.setCreateDate(LocalDateTime.now());
     // 소셜 로그인 사용자 정보를 저장 또는 업데이트합니다.
     return memberRepository.save(member);
 
@@ -72,4 +83,101 @@ public class MemberService {
     Optional<Member> existingMember = memberRepository.findByloginId(loginId);
     return existingMember.isPresent();
   }
+
+
+  // 소셜로그인 닉네임 업데이트 메서드 추가
+  public void updateNickname(String loginId, String newNickname, String socialProvider, LocalDateTime localDateTime) {
+
+    if (loginId == null) {
+      throw new DataNotFoundException("socialLoginId not found in session");
+    }
+    // 세션에서 가져온 아이디로 데이터 조회
+    Member member = new Member();
+    member.setLoginId(loginId);
+    member.setNickName(newNickname);
+    member.setSocialProvider(socialProvider);
+    member.setCreateDate(LocalDateTime.now());
+    memberRepository.save(member);
+  }
+
+
+  @Transactional
+  public Member updateMember(String loginId, String nickName, String phone, String email, LocalDateTime createDate) {
+    // 트랜잭션 내에서 일어나는 작업
+    Optional<Member> existingMember = memberRepository.findByloginId(loginId);
+
+    Member member = null;
+    if (existingMember.isPresent()) {
+      member = existingMember.get();
+      member.setNickName(nickName);
+      member.setPhone(phone);
+      member.setEmail(email);
+      member.setCreateDate(createDate);
+      memberRepository.save(member);
+    }
+    return member;
+    // 다른 작업들...
+  }
+
+  //마이페이지 휴대전화 수정로직
+  public Member updateMemberPhone(String loginId, String phone) {
+    // 트랜잭션 내에서 일어나는 작업
+    Optional<Member> existingMember = memberRepository.findByloginId(loginId);
+
+    Member member = null;
+    if (existingMember.isPresent()) {
+      member = existingMember.get();
+      member.setPhone(phone);
+      memberRepository.save(member);
+    }
+    return member;
+    // 다른 작업들...
+  }
+
+  public Member updateMemberemail(String loginId, String email) {
+    // 트랜잭션 내에서 일어나는 작업
+    Optional<Member> existingMember = memberRepository.findByloginId(loginId);
+
+    Member member = null;
+    if (existingMember.isPresent()) {
+      member = existingMember.get();
+      member.setEmail(email);
+      memberRepository.save(member);
+    }
+    return member;
+  }
+
+  // 비밀번호 업데이트 메서드
+  public void updateMemberPassword(Member member) {
+    // 여기에서는 간단하게 JPA를 사용하여 업데이트하도록 예시를 작성했습니다.
+    // 데이터베이스에서 해당 멤버를 조회
+    Member existingMember = memberRepository.findById(member.getId()).orElse(null);
+    if (existingMember != null) {
+      // 새로운 비밀번호로 업데이트
+      existingMember.setPassword(member.getPassword());
+      // 실제 데이터베이스에 업데이트
+      memberRepository.save(existingMember);
+    } else {
+      // 해당 ID의 멤버가 존재하지 않는 경우 예외 처리 또는 로깅 등을 수행
+      throw new DataNotFoundException("Member not found");
+    }
+  }
+  public List<Member> getMembersByEmail(String email) {
+    List<Member> members = this.memberRepository.findByEmail(email);
+    if (!members.isEmpty()) {
+      return members;
+    } else {
+      throw new DataNotFoundException("members not found for email: " + email);
+    }
+  }
+  public List<Member> getMembersByPhone(String phone){
+    List<Member> members = this.memberRepository.findByPhone(phone);
+
+    if (!members.isEmpty()){
+      return members;
+    }else {
+      throw new DataNotFoundException("members not found for phone: " + phone);
+    }
+  }
 }
+
