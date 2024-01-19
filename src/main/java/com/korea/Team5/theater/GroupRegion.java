5ce3ecd66cc2cf22fc01c596d358f70438ff0f98
sang5517 <143565832+sang5517@users.noreply.github.com>
2024-01-19 15:45:07 +0900

    Enhancement/2 (#40)
    * login complete test
    
    * close test
    
    * 1218login
    
    * 1218ilha
    
    * hijfskjf
    
    * 1219review
    
    * 1219modifyexception
    
    * 1220
    
    * 1220delete
    
    * 1221fsdfsdf
    
    * 1222
    
    * 1226ilha
    
    * 1227review
    
    * 1227clear
    
    * 1227lastclear
    
    * 1229
    
    * 1229mypage
    
    * 1229clear
    
    * 0102
    
    * 0102email
    
    * 0103modify
    
    * 0103clear
    
    * 0104
    
    * 0104clear
    
    * 0105
    
    * 0105mypagepassword
    
    * 0105last
    
    * 0108admin
    
    * 0109mypagereview
    
    * 0110
    
    * 0110ilha
    
    * 0111
    
    * 0111clear
    
    * 0112ilha
    
    * 0115ilha
    
    * 0116ilha
    
    * 0117liha
    
    * 0117modify
    
    * 0118
    
    * 0117clear
    
    * 0118modify
    
    * 0119ilha

--- src/main/java/com/korea/Team5/theater/GroupRegion.java
+++ src/main/java/com/korea/Team5/theater/GroupRegion.java
@@ -8,7 +8,10 @@ import java.util.*;
 public class GroupRegion {
 
   static final private Map<String, List<String>> groupMap;
-
+  static final private List<String> cityList = new ArrayList<>(Arrays.asList("대전시", "세종시", "부산시", "대구시"));;
+  static final private List<String> areaList = new ArrayList<>(Arrays.asList("경상남도", "경상북도", "충청북도", "충청남도"));;
+  static final private List<String> smalltargetList = new ArrayList<>(Arrays.asList("부산시 + 구","대구시 + 구","부산시 + 군","대구시 + 군",
+          "대전시 + 구","세종시 + 구","대전시 + 군","세종시 + 군"));
   static {
     String chungcheongGroup = "대전/충청/세종";
     String kyeongsangGroup = "부산/대구/경상";
@@ -18,12 +21,18 @@ public class GroupRegion {
     List<String> kyeongsangList = new ArrayList<>(Arrays.asList("부산시", "대구시", "경상남도", "경상북도"));
     List<String> jeonlaList = new ArrayList<>(Arrays.asList("광주시", "전라북도", "전라남도"));
 
+
+
     groupMap = new HashMap<>();
     groupMap.put(chungcheongGroup, chungcheongList);
     groupMap.put(kyeongsangGroup, kyeongsangList);
     groupMap.put(jeonlaGroup, jeonlaList);
 
+
   }
+
+
+
 
   static Map<String, List<String>> getGroupMap() {
     return groupMap;
@@ -48,4 +57,15 @@ public class GroupRegion {
     return groupMap.get(groupName);
   }
 
+  static List<String> getCityList() {
+    return cityList;
+  }
+
+  static List<String> getAreaList() {
+    return areaList;
+  }
+  static List<String> getSmalltargetList(){
+
+    return smalltargetList;
+  }
 }
