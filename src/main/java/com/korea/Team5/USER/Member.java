a2cdc315e81330146f357fbc5ead3b0fe116d102
habinnn <107250227+habinnn@users.noreply.github.com>
2024-01-24 12:10:33 +0900

    Work (#45)
    * habin task
    
    * habin
    
    * 1220habin
    
    * 1221habin
    
    * 1226api
    
    * 1226api
    
    * s
    
    * 1228habin
    
    * s
    
    * habin
    
    * habin
    
    * s
    
    * s
    
    * s
    
    * s
    
    * s

--- src/main/java/com/korea/Team5/USER/Member.java
+++ src/main/java/com/korea/Team5/USER/Member.java
@@ -1,5 +1,6 @@
 package com.korea.Team5.USER;
 import com.korea.Team5.Review.Review;
+import com.korea.Team5.board.article.Article;
 import com.korea.Team5.movie.entity.Movie;
 import com.korea.Team5.movie.entity.MovieInfo;
 import jakarta.persistence.*;
@@ -46,4 +47,7 @@ public class Member {
   private List<Review> reviewList;
 
   private String role;
+
+  @OneToMany(mappedBy = "member")
+  private List<Article> articleList;
 }
